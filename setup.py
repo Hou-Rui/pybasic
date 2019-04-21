ce3862f57f77980523e294d465ddfa5216633864
Hou-Rui <13244639785@163.com>
2019-04-21 12:58:56 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author='Hou Rui',
     author_email='13244639785@163.com',
     keywords='ply python BASIC interpreter',
+    long_description=open('README.md').read(),
+    url='https://github.com/Hou-Rui/pybasic',
     install_requires=[
         'ply>=3.0'
     ],
