6e7354d184c1be4fea3ffd76cf52dfb6fb1ce209
turmary <turmary@126.com>
2023-07-26 00:01:22 +0800

    Update: package version to 1.1

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='ply-pybasic',
-    version='1.0',
+    version='1.1',
     packages=['pybasic'],
-    author='Hou Rui',
-    author_email='13244639785@163.com',
+    author='Hou Rui, Peter Yang',
+    author_email='13244639785@163.com,turmary@126.com',
     keywords='ply python BASIC interpreter',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/Hou-Rui/pybasic',
     install_requires=[
         'ply>=3.0'
